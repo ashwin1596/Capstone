cc68012df416839d24224ea82c1d4f02c11dc58a
Ashwin Sharad Kherde <ak6913@rit.edu>
2025-02-05 00:53:38 -0500

    added testrun for Model1

--- src/models.py
+++ src/models.py
@@ -27,7 +27,8 @@ class ConvNetModel(nn.Module):
             if layer == "M":
                 layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
             else:
-                layers.append(nn.Conv2d(in_channels=in_channels, out_channels=layer, kernel_size=(3,3), padding=1))
+                out_channels = int(layer)
+                layers.append(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3,3), padding=1))
                 layers.append(nn.BatchNorm2d(num_features=layer))
                 layers.append(nn.ReLU(inplace=True))
                 in_channels = layer
@@ -43,19 +44,22 @@ class ConvNetModel(nn.Module):
 
         # calculating input features based on maxpool layers in cnn and input size
         current_size = self.input_size
+        last_conv_channels = None
+
         for layer in self.cnn_plan:
             if layer == "M":
                 current_size //= 2
+            else:
+                last_conv_channels = int(layer)
 
-        in_features = self.cnn_plan[-1] * current_size * current_size
+        in_features = last_conv_channels * current_size * current_size
 
-        layers.append()
-        for layer in range(self.fc_plan):
+        for layer in self.fc_plan:
             layers.append(nn.Linear(in_features, layer)) 
             layers.append(nn.ReLU(inplace=True))
             in_features = layer
         
-        layers.append(nn.Linear(in_features, num_classes))
+        layers.append(nn.Linear(in_features, self.num_classes))
 
         return nn.Sequential(*layers)
 
@@ -75,6 +79,6 @@ class Model1(ConvNetModel):
     CNN_PLAN = [64, "M", 128, "M", 256, 256, "M", 512, 512, "M"]  # Example CNN plan
     FC_PLAN = [4096, 4096]  # Example FC plan
 
-    def __init__(sel, **kwargs):
-        super().__init__(self.PLAN, self.FC_PLAN, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(self.CNN_PLAN, self.FC_PLAN, **kwargs)
 
